a5e4e649cd4fb4b380c219fab704e6e0a726c6fe
manishsaraan <manish021js@gmail.com>
2018-05-04 19:05:40 +0530

    Gravatar tests updated

--- enzyme/test/gravatar.spec.js
+++ enzyme/test/gravatar.spec.js
@@ -5,11 +5,35 @@ import md5 from 'md5';
 import Gravatar from '../lib/gravatar';
 import Avatar from '../lib/avatar';
 import Email from '../lib/email';
+import { wrap } from 'module';
 
 describe('<Gravatar/>', () => {
-    it('it contains a <Avatar/>', () => {
-       const wrapper = mount(<Gravatar/>);
+    const wrapper = mount(<Gravatar/>);
+
+    it('it contains a <Avatar/> component', () => {
+      
        expect(wrapper.find(Avatar)).to.have.length(1);
     });
-    
-})
+
+    it('it contains a <Email/> component', () => {
+      
+       expect(wrapper.find(Email)).to.have.length(1);
+    });
+
+    it('should have an initial email state', () => {
+        
+        expect(wrapper.state().email).to.equal('someone@example.com');
+    });
+
+    it('should have an initial src state', () => {
+
+        expect(wrapper.state().src).to.equal('http://placehold.it/200x200');
+    });
+
+    it('should update the src on clicking the fetch', () => {
+        wrapper.setState({email: 'hello@mail.com'});
+        wrapper.find('button').simulate('click');
+        expect(wrapper.state('email')).to.equal('hello@mail.com');
+        expect(wrapper.state('src')).to.equal(`http://gravatar.com/avatar/${md5('hello@mail.com')}?s=200`)        
+    });
+});
