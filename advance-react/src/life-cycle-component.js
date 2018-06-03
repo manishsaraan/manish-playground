a13fd9de635e71f4c56a6a224128f2a73c032ad6
manishsaraan <manish021js@gmail.com>
2018-06-03 23:12:24 +0530

    LifeCycle Component created

---
+++ advance-react/src/life-cycle-component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class LifeCycleMethods extends Component {
+  
+  render(){
+      return(
+          <div>Helloe</div>
+      );
+  }
+}
+
+export default LifeCycleMethods;
