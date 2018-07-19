d0a3dd53510e80fde1e851bfa1cd9fc0ecd0ca39
manishsaraan <manish021js@gmail.com>
2018-07-19 22:35:30 +0530

    collection of anyting in array

---
+++ JS-Weired-Parts/functions and objecgts/collecton_of_anything.js
@@ -0,0 +1,16 @@
+var arr = [
+    1,
+    false,
+    {
+        name: 'tony',
+        address: '111 main st'
+    },
+    function(name){
+        var greeting = 'hello';
+        console.log(greeting + ' ' + name);
+    },
+    "hello"
+];
+
+console.log(arr);
+arr[3](arr[2].name);
