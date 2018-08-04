3cf0e6f0adf8229e9bb5bea2d0078af561397799
manishsaraan <manish021js@gmail.com>
2018-08-04 16:30:00 +0530

    Module pattern in js

---
+++ Understanding-NodeJs/module exports and require/module patterns/index.js
@@ -0,0 +1,10 @@
+var greet  = require('./greet');
+var greet2 = require('./greet2').greet;
+var greet3 = require('./greet3');
+var greet4 = require('./greet4');
+var greet5 = require('./greet5');
+var gtr = new greet4();
+gtr.greet();
+greet();
+greet3.greet();
+greet5.greet();
