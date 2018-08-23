6369f6ee4aa9030cfa5e132819373c5326d74a93
manishsaraan <manish021js@gmail.com>
2018-08-23 20:16:13 +0530

    returning json response in node server

---
+++ Understanding-NodeJs/HTTP and being a Web Server/json_server.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(req, res){
+  
+    res.writeHead(200, { 'Content-Type': 'application/json'});
+    var obj = {
+        firstName: 'John',
+        lastName: 'Doe'
+    };
+
+    res.end(JSON.stringify(obj));
+}).listen(1337);
